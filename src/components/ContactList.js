259e4350605c25ec2109d4c524f275cffeb1d7ef
h_latifi8 <h.latifi03@gmail.com>
2021-09-23 21:31:48 +0330

    add searchBar and allcontacts state

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -6,11 +6,14 @@ import Contact from './Contact'
 
 function ContactList({props}) {
     const [contacts, setContacts] = useState([]);
+    const [allContacts , setAllContacts]=useState([]);
+    const [search,setSearch]=useState('')
 
     useEffect(() => {
         const getContacts = async () => {
             const { data } = await getAllContacts();
             setContacts(data)
+            setAllContacts(data)
         }
         try {
             getContacts();
@@ -20,6 +23,18 @@ function ContactList({props}) {
         }
 
     }, [])
+    const searchHandler=(e)=>{
+        setSearch(e.target.value);
+        if(e.target.value!==''){
+            const filter= allContacts.filter(c=>{
+                return Object.values(c).join(" ").toLowerCase().includes(search.toLowerCase());
+            })
+            setContacts(filter)
+        }else{
+            setContacts(allContacts)
+        }
+        
+    }
 
     const deleteContact = async (id) => {
 
@@ -31,6 +46,9 @@ function ContactList({props}) {
     return (
         <section className="ContactList">
             <button> <Link to="/add">Add new Contact</Link> </button>
+            <div className="SearchBar">
+                <input type="search" value={search} onChange={searchHandler}></input>
+            </div>
             {contacts.map(contact => {
                 return (
                     <Contact key={contact.id} contact={contact} onDelete={deleteContact} />
